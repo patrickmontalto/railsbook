2ab6ee10b9c048487630ea96e547f8744ec62f07
patrickmontalto <pmontalto@gmail.com>
2015-12-10 10:24:05 -0500

    user feed

--- test/features/post_feed_test.rb
+++ test/features/post_feed_test.rb
@@ -3,6 +3,9 @@ require "test_helper"
 feature "PostFeed" do
   scenario "user's home page has a feed of friend's posts" do
     log_in_user
+    page.must_have_content("User one")
+    page.must_have_content("Patrick M")
     page.must_have_content("This is a post.")
+    page.must_have_content("This post is great.")
   end
 end
